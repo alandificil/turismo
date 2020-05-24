38bb303b53fa0adc32b779c38e0a5fb2fef2de14
Alan <alanmauriciodossantos@gmail.com>
2020-05-24 20:46:09 -0300

    criando um menu

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> ta bom
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+       
+        <a className="navbar-brand" href="#"><i className="fas fa-globe-americas"></i>&nbsp;Coqueiros Turismo</a>
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+        aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>   
+       
+        
+        <section className="collapse navbar-collapse"  id="navbarMainToggler">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="#">Home</a>
+            <a className="nav-item nav-link" href="#">Onde ir</a>
+            <a className="nav-item nav-link" href="#">Contato</a>
+          </div>
+        </section>
+
+      </nav>
     </div>
   );
 }
